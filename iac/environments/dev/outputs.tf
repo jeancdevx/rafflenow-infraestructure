d571705a0dbc9dbca27fcbbff22ff3756d644a9a
jeancdev <jcode2006@gmail.com>
2025-10-21 08:21:19 -0500

    feat: añadir salidas para S3, DynamoDB y lambdas

--- iac/environments/dev/outputs.tf
+++ iac/environments/dev/outputs.tf
@@ -17,3 +17,23 @@ output "aws_profile" {
   description = "El perfil de AWS utilizado"
   value       = var.aws_profile
 }
+
+output "bucket_name" {
+  description = "The name of the S3 assets bucket"
+  value       = module.storage.s3_assets_bucket_name
+}
+
+output "table_name" {
+  description = "The name of the DynamoDB raffles table"
+  value       = module.storage.dynamodb_raffles_table_name
+}
+
+output "lambda_list_raffles_name" {
+  description = "Name of list raffles Lambda"
+  value       = module.compute.lambda_list_raffles_name
+}
+
+output "lambda_list_raffles_arn" {
+  description = "ARN of list raffles Lambda"
+  value       = module.compute.lambda_list_raffles_arn
+}
