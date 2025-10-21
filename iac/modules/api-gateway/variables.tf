ee033c72832ac95c5a3033f5654748620a7df476
jeancdev <jcode2006@gmail.com>
2025-10-21 18:20:29 -0500

    feat: agregar variables para la función Lambda de creación de rifas

--- iac/modules/api-gateway/variables.tf
+++ iac/modules/api-gateway/variables.tf
@@ -23,3 +23,18 @@ variable "lambda_list_raffles_name" {
   description = "Name of the list raffles Lambda function"
   type        = string
 }
+
+variable "lambda_create_raffle_arn" {
+  description = "ARN of the create raffle Lambda function"
+  type        = string
+}
+
+variable "lambda_create_raffle_name" {
+  description = "Name of the create raffle Lambda function"
+  type        = string
+}
+
+variable "lambda_create_raffle_invoke_arn" {
+  description = "Invoke ARN of the create raffle Lambda function"
+  type        = string
+}
