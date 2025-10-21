72ce82c3e5d6926059ac4a6e5b8fc37bfb4441d9
jeancdev <jcode2006@gmail.com>
2025-10-20 22:10:07 -0500

    chore: añadir estructura inicial de terraform

---
+++ iac/environments/dev/variables.tf
@@ -0,0 +1,83 @@
+variable "aws_region" {
+  description = "Region de aws"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "environment" {
+  description = "Nombre de entorno"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Nombre del proyecto"
+  type        = string
+  default     = "rafflenow"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR de la VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "Zonas de disponibilidad"
+  type        = list(string)
+  default     = ["us-east-2a", "us-east-2b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR de subredes publicas"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR de subredes privadas"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.20.0/24"]
+}
+
+variable "api_throttle_rate" {
+  description = "Tasa de limitacion de API Gateway"
+  type        = number
+  default     = 100
+}
+
+variable "api_throttle_burst" {
+  description = "Límite de sobrecarga de API Gateway"
+  type        = number
+  default     = 200
+}
+
+variable "lambda_memory_size" {
+  description = "Tamaño de memoria para las funciones Lambda"
+  type        = number
+  default     = 512
+}
+
+variable "lambda_timeout" {
+  description = "Timeout para las funciones Lambda"
+  type        = number
+  default     = 30
+}
+
+variable "dynamodb_billing_mode" {
+  description = "Modo de facturación de DynamoDB"
+  type        = string
+  default     = "PAY_PER_REQUEST"
+}
+
+variable "dynamodb_read_capacity" {
+  description = "Unidades de capacidad de lectura de DynamoDB (solo para modo PROVISIONED)"
+  type        = number
+  default     = 5
+}
+
+variable "dynamodb_write_capacity" {
+  description = "Unidades de capacidad de escritura de DynamoDB (solo para modo PROVISIONED)"
+  type        = number
+  default     = 5
+}
