038561cb605ec6c14a2929c972d6768807cbfb24
jeancdev <jcode2006@gmail.com>
2025-10-21 18:22:19 -0500

    feat: agregar método y integración POST para la creación de rifas en la API

--- iac/modules/api-gateway/main.tf
+++ iac/modules/api-gateway/main.tf
@@ -1,4 +1,3 @@
-# API Gateway REST API
 resource "aws_api_gateway_rest_api" "rafflenow_api" {
   name        = "${var.name_prefix}-api"
   description = "RaffleNow REST API for managing raffles"
@@ -13,14 +12,12 @@ resource "aws_api_gateway_rest_api" "rafflenow_api" {
   })
 }
 
-# Resource: /api
 resource "aws_api_gateway_resource" "api" {
   rest_api_id = aws_api_gateway_rest_api.rafflenow_api.id
   parent_id   = aws_api_gateway_rest_api.rafflenow_api.root_resource_id
   path_part   = "api"
 }
 
-# Resource: /api/v1
 resource "aws_api_gateway_resource" "v1" {
   rest_api_id = aws_api_gateway_rest_api.rafflenow_api.id
   parent_id   = aws_api_gateway_resource.api.id
@@ -61,6 +58,33 @@ resource "aws_lambda_permission" "api_gateway_invoke_list_raffles" {
   source_arn    = "${aws_api_gateway_rest_api.rafflenow_api.execution_arn}/*/*"
 }
 
+# Method: POST /api/v1/raffles
+resource "aws_api_gateway_method" "post_raffles" {
+  rest_api_id   = aws_api_gateway_rest_api.rafflenow_api.id
+  resource_id   = aws_api_gateway_resource.raffles.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+# Integration: POST /api/v1/raffles -> Lambda create-raffle
+resource "aws_api_gateway_integration" "post_raffles_lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.rafflenow_api.id
+  resource_id             = aws_api_gateway_resource.raffles.id
+  http_method             = aws_api_gateway_method.post_raffles.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_create_raffle_invoke_arn
+}
+
+# Permission: Allow API Gateway to invoke create-raffle Lambda
+resource "aws_lambda_permission" "api_gateway_invoke_create_raffle" {
+  statement_id  = "AllowAPIGatewayInvokeCreateRaffle"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_create_raffle_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.rafflenow_api.execution_arn}/*/*"
+}
+
 # Data source for current region
 data "aws_region" "current" {}
 
@@ -74,7 +98,9 @@ resource "aws_api_gateway_deployment" "api_deployment" {
       aws_api_gateway_resource.v1.id,
       aws_api_gateway_resource.raffles.id,
       aws_api_gateway_method.get_raffles.id,
+      aws_api_gateway_method.post_raffles.id,
       aws_api_gateway_integration.get_raffles_lambda.id,
+      aws_api_gateway_integration.post_raffles_lambda.id,
     ]))
   }
 
