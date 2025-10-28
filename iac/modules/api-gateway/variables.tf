263fae5ed5857ea2a547c82dd7d8fe32705f3e62
jeancdev <jcode2006@gmail.com>
2025-10-28 16:09:31 -0500

    feat: agregar endpoint POST /raffles/{id}/participate en API Gateway

--- iac/modules/api-gateway/variables.tf
+++ iac/modules/api-gateway/variables.tf
@@ -38,3 +38,18 @@ variable "lambda_create_raffle_invoke_arn" {
   description = "Invoke ARN of the create raffle Lambda function"
   type        = string
 }
+
+variable "lambda_ingest_participation_arn" {
+  description = "ARN of the ingest participation Lambda function"
+  type        = string
+}
+
+variable "lambda_ingest_participation_name" {
+  description = "Name of the ingest participation Lambda function"
+  type        = string
+}
+
+variable "lambda_ingest_participation_invoke_arn" {
+  description = "Invoke ARN of the ingest participation Lambda function"
+  type        = string
+}
