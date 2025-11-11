18870f26729d2a044b224e7a01efd8ffbba71ec0
jeancdev <jcode2006@gmail.com>
2025-11-11 16:40:55 -0500

    refactor(compute): extraer configuración IAM a iam.tf
    Separa roles, policies y attachments en archivo dedicado:
    - Lambda execution role
    - DynamoDB policy
    - SQS policy
    - S3 policy

---
+++ iac/modules/compute/iam.tf
@@ -0,0 +1,105 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.name_prefix}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-lambda-role"
+    Type = "IAM"
+  })
+}
+
+resource "aws_iam_policy" "lambda_dynamodb_policy" {
+  name = "${var.name_prefix}-lambda-dynamodb-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:Scan",
+          "dynamodb:Query",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+        ]
+        Resource = [
+          "arn:aws:dynamodb:*:*:table/${var.dynamodb_table_name}",
+          "arn:aws:dynamodb:*:*:table/${var.dynamodb_table_name}/index/*",
+          "arn:aws:dynamodb:*:*:table/${var.dynamodb_participants_table_name}",
+          "arn:aws:dynamodb:*:*:table/${var.dynamodb_participants_table_name}/index/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
+}
+
+resource "aws_iam_policy" "lambda_sqs_policy" {
+  name = "${var.name_prefix}-lambda-sqs-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:SendMessage",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes",
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_sqs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_sqs_policy.arn
+}
+
+resource "aws_iam_policy" "lambda_s3_policy" {
+  name = "${var.name_prefix}-lambda-s3-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+        ]
+        Resource = "${var.s3_assets_bucket_arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_s3_policy.arn
+}
