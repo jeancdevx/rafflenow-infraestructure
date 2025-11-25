c97aa2d066ee5e0b24244ffba8e61c161229d328
jeancdev <jcode2006@gmail.com>
2025-11-25 17:22:42 -0500

    feat(create-raffle): configurar AWS Lambda Powertools

--- app/lambdas/create-raffle/lib/powertools.js
+++ app/lambdas/create-raffle/lib/powertools.js
@@ -1,18 +1,18 @@
-import { Logger } from "@aws-lambda-powertools/logger";
-import { Tracer } from "@aws-lambda-powertools/tracer";
-import { Metrics } from "@aws-lambda-powertools/metrics";
+import { Logger } from '@aws-lambda-powertools/logger'
+import { Tracer } from '@aws-lambda-powertools/tracer'
+import { Metrics } from '@aws-lambda-powertools/metrics'
 
 export const logger = new Logger({
-  serviceName: "create-raffle",
-  logLevel: process.env.LOG_LEVEL || "INFO",
-});
+  serviceName: 'create-raffle',
+  logLevel: process.env.LOG_LEVEL || 'INFO'
+})
 
 export const tracer = new Tracer({
-  serviceName: "create-raffle",
-  captureHTTPsRequests: true,
-});
+  serviceName: 'create-raffle',
+  captureHTTPsRequests: true
+})
 
 export const metrics = new Metrics({
-  namespace: "RaffleNow",
-  serviceName: "create-raffle",
-});
+  namespace: 'RaffleNow',
+  serviceName: 'create-raffle'
+})
