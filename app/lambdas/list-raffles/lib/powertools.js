f55823a5bf7a4ad9dc069d958ec5dd0b9c8cef7c
jeancdev <jcode2006@gmail.com>
2025-11-25 17:31:09 -0500

    feat(list-raffles): configurar AWS Lambda Powertools

--- app/lambdas/list-raffles/lib/powertools.js
+++ app/lambdas/list-raffles/lib/powertools.js
@@ -1,18 +1,18 @@
-import { Logger } from "@aws-lambda-powertools/logger";
-import { Tracer } from "@aws-lambda-powertools/tracer";
-import { Metrics } from "@aws-lambda-powertools/metrics";
+import { Logger } from '@aws-lambda-powertools/logger'
+import { Tracer } from '@aws-lambda-powertools/tracer'
+import { Metrics } from '@aws-lambda-powertools/metrics'
 
 export const logger = new Logger({
-  serviceName: "list-raffles",
-  logLevel: process.env.LOG_LEVEL || "INFO",
-});
+  serviceName: 'list-raffles',
+  logLevel: process.env.LOG_LEVEL || 'INFO'
+})
 
 export const tracer = new Tracer({
-  serviceName: "list-raffles",
-  captureHTTPsRequests: true,
-});
+  serviceName: 'list-raffles',
+  captureHTTPsRequests: true
+})
 
 export const metrics = new Metrics({
-  namespace: "RaffleNow",
-  serviceName: "list-raffles",
-});
+  namespace: 'RaffleNow',
+  serviceName: 'list-raffles'
+})
