4072b6da7cf3781091ec97f7cf1608d78422c4f1
jeancdev <jcode2006@gmail.com>
2025-11-25 18:10:13 -0500

    feat(upload-image): configurar AWS Lambda Powertools

--- app/lambdas/upload-image/lib/powertools.js
+++ app/lambdas/upload-image/lib/powertools.js
@@ -1,19 +1,19 @@
-import { Logger } from "@aws-lambda-powertools/logger";
-import { Tracer } from "@aws-lambda-powertools/tracer";
-import { Metrics } from "@aws-lambda-powertools/metrics";
+import { Logger } from '@aws-lambda-powertools/logger'
+import { Tracer } from '@aws-lambda-powertools/tracer'
+import { Metrics } from '@aws-lambda-powertools/metrics'
 
 const logger = new Logger({
-  serviceName: "upload-image",
-  logLevel: process.env.LOG_LEVEL || "INFO",
-});
+  serviceName: 'upload-image',
+  logLevel: process.env.LOG_LEVEL || 'INFO'
+})
 
 const tracer = new Tracer({
-  serviceName: "upload-image",
-});
+  serviceName: 'upload-image'
+})
 
 const metrics = new Metrics({
-  namespace: "RaffleNow",
-  serviceName: "upload-image",
-});
+  namespace: 'RaffleNow',
+  serviceName: 'upload-image'
+})
 
-export { logger, tracer, metrics };
+export { logger, tracer, metrics }
