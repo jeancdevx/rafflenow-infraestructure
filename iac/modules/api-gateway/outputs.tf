d87f9cbf237d703c4b9cabed6aa830ae90bd9aba
jeancdev <jcode2006@gmail.com>
2025-11-30 18:42:43 -0500

    feat(monitoring): add CloudWatch access logs and IAM roles for API Gateway

--- iac/modules/api-gateway/outputs.tf
+++ iac/modules/api-gateway/outputs.tf
@@ -81,3 +81,23 @@ output "authenticated_api_stage_name" {
   description = "Stage name of the Authenticated API Gateway"
   value       = aws_api_gateway_stage.authenticated_api_stage.stage_name
 }
+
+output "public_api_access_logs_arn" {
+  description = "ARN of the Public API Gateway access logs CloudWatch Log Group"
+  value       = aws_cloudwatch_log_group.public_api_access_logs.arn
+}
+
+output "public_api_access_logs_name" {
+  description = "Name of the Public API Gateway access logs CloudWatch Log Group"
+  value       = aws_cloudwatch_log_group.public_api_access_logs.name
+}
+
+output "authenticated_api_access_logs_arn" {
+  description = "ARN of the Authenticated API Gateway access logs CloudWatch Log Group"
+  value       = aws_cloudwatch_log_group.authenticated_api_access_logs.arn
+}
+
+output "authenticated_api_access_logs_name" {
+  description = "Name of the Authenticated API Gateway access logs CloudWatch Log Group"
+  value       = aws_cloudwatch_log_group.authenticated_api_access_logs.name
+}
