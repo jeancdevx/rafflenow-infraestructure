44b911ca9c0b27e26af987676c09f778a933196a
jeancdev <jcode2006@gmail.com>
2025-11-30 11:59:13 -0500

    feat(cognito): add function to add user to Cognito group

---
+++ app/lambdas/post-confirmation/lib/services/cognito-service.js
@@ -0,0 +1,24 @@
+import { AdminAddUserToGroupCommand } from '@aws-sdk/client-cognito-identity-provider'
+import { cognitoClient } from '../clients.js'
+import { logger } from '../powertools.js'
+
+export async function addUserToGroup(userPoolId, username, groupName) {
+  logger.info('Adding user to group', {
+    userPoolId,
+    username,
+    groupName
+  })
+
+  const command = new AdminAddUserToGroupCommand({
+    UserPoolId: userPoolId,
+    Username: username,
+    GroupName: groupName
+  })
+
+  await cognitoClient.send(command)
+
+  logger.info('User added to group successfully', {
+    username,
+    groupName
+  })
+}
