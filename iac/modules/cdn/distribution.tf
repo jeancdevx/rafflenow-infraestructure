8145df24b51de67bc6e441ddfd7b6f07aee2e3d1
jeancdev <jcode2006@gmail.com>
2025-11-30 21:59:48 -0500

    feat(cloudfront): add API Gateway origins and related variables for CloudFront distribution

--- iac/modules/cdn/distribution.tf
+++ iac/modules/cdn/distribution.tf
@@ -1,4 +1,3 @@
-# CloudFront Distribution
 resource "aws_cloudfront_distribution" "assets_cdn" {
   enabled             = true
   is_ipv6_enabled     = true
@@ -7,6 +6,8 @@ resource "aws_cloudfront_distribution" "assets_cdn" {
   default_root_object = "index.html"
   price_class         = "PriceClass_200"
   web_acl_id          = var.web_acl_id
+
+  aliases = var.domain_name != null ? [var.domain_name, "www.${var.domain_name}"] : []
 
   origin {
     domain_name = var.assets_bucket_regional_domain_name
@@ -25,6 +26,64 @@ resource "aws_cloudfront_distribution" "assets_cdn" {
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.assets_oai.cloudfront_access_identity_path
     }
+  }
+
+  origin {
+    domain_name = var.api_gateway_public_domain
+    origin_id   = "API-Public"
+    origin_path = "/${var.api_gateway_public_stage}"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+
+    custom_header {
+      name  = "X-Origin-Verify"
+      value = var.origin_verify_header_value
+    }
+  }
+
+  origin {
+    domain_name = var.api_gateway_authenticated_domain
+    origin_id   = "API-Authenticated"
+    origin_path = "/${var.api_gateway_authenticated_stage}"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+
+    custom_header {
+      name  = "X-Origin-Verify"
+      value = var.origin_verify_header_value
+    }
+  }
+
+  ordered_cache_behavior {
+    path_pattern             = "/api/v1/public/*"
+    allowed_methods          = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods           = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id         = "API-Public"
+    cache_policy_id          = data.aws_cloudfront_cache_policy.caching_disabled.id
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer_except_host_header.id
+    viewer_protocol_policy   = "redirect-to-https"
+    compress                 = true
+  }
+
+  ordered_cache_behavior {
+    path_pattern             = "/api/v1/*"
+    allowed_methods          = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods           = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id         = "API-Authenticated"
+    cache_policy_id          = data.aws_cloudfront_cache_policy.caching_disabled.id
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer_except_host_header.id
+    viewer_protocol_policy   = "redirect-to-https"
+    compress                 = true
   }
 
   ordered_cache_behavior {
@@ -55,15 +114,25 @@ resource "aws_cloudfront_distribution" "assets_cdn" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
-    # TODO: Para usar dominio custom, agregar:
-    # acm_certificate_arn      = var.acm_certificate_arn
-    # ssl_support_method       = "sni-only"
-    # minimum_protocol_version = "TLSv1.2_2021"
+    cloudfront_default_certificate = var.acm_certificate_arn == null
+    acm_certificate_arn            = var.acm_certificate_arn
+    ssl_support_method             = var.acm_certificate_arn != null ? "sni-only" : null
+    minimum_protocol_version       = var.acm_certificate_arn != null ? "TLSv1.2_2021" : null
   }
 
   tags = merge(var.tags, {
     Name = "${var.name_prefix}-assets-cdn"
     Type = "CloudFront"
   })
+}
+
+# Use AWS managed policies for API Gateway
+# CachingDisabled: 4135ea2d-6df8-44a3-9df3-4b5a84be39ad
+# AllViewerExceptHostHeader: b689b0a8-53d0-40ab-baf2-68738e2966ac
+data "aws_cloudfront_cache_policy" "caching_disabled" {
+  name = "Managed-CachingDisabled"
+}
+
+data "aws_cloudfront_origin_request_policy" "all_viewer_except_host_header" {
+  name = "Managed-AllViewerExceptHostHeader"
 }
