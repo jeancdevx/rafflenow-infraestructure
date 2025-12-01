ca80960acbaaacf3e7739ef20201fb7ee93804ba
jeancdev <jcode2006@gmail.com>
2025-12-01 10:24:54 -0500

    feat(permissions): expand IAM role policies for enhanced resource access

--- iac/modules/github-oidc/main.tf
+++ iac/modules/github-oidc/main.tf
@@ -49,11 +49,14 @@ resource "aws_iam_role_policy" "terraform_permissions" {
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject",
-          "s3:ListBucket"
+          "s3:ListBucket",
+          "s3:GetBucketNotification",
+          "s3:PutBucketNotification"
         ]
         Resource = [
           "arn:aws:s3:::${var.terraform_state_bucket}",
-          "arn:aws:s3:::${var.terraform_state_bucket}/*"
+          "arn:aws:s3:::${var.terraform_state_bucket}/*",
+          "arn:aws:s3:::*"
         ]
       },
       {
@@ -67,7 +70,7 @@ resource "aws_iam_role_policy" "terraform_permissions" {
         Resource = "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${var.terraform_lock_table}"
       },
       {
-        Sid    = "TerraformPlanPermissions"
+        Sid    = "IAMPermissions"
         Effect = "Allow"
         Action = [
           "iam:GetRole",
@@ -77,16 +80,46 @@ resource "aws_iam_role_policy" "terraform_permissions" {
           "iam:ListRolePolicies",
           "iam:ListAttachedRolePolicies",
           "iam:ListInstanceProfilesForRole",
+          "iam:GetOpenIDConnectProvider",
+          "iam:ListOpenIDConnectProviders"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "LambdaPermissions"
+        Effect = "Allow"
+        Action = [
           "lambda:GetFunction",
           "lambda:GetFunctionConfiguration",
           "lambda:GetPolicy",
           "lambda:ListVersionsByFunction",
-          "lambda:GetFunctionCodeSigningConfig",
-          "apigateway:GET",
+          "lambda:GetFunctionCodeSigningConfig"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "APIGatewayPermissions"
+        Effect = "Allow"
+        Action = [
+          "apigateway:GET"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "DynamoDBPermissions"
+        Effect = "Allow"
+        Action = [
           "dynamodb:DescribeTable",
           "dynamodb:DescribeContinuousBackups",
           "dynamodb:DescribeTimeToLive",
-          "dynamodb:ListTagsOfResource",
+          "dynamodb:ListTagsOfResource"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "S3Permissions"
+        Effect = "Allow"
+        Action = [
           "s3:GetBucketPolicy",
           "s3:GetBucketAcl",
           "s3:GetBucketCORS",
@@ -99,29 +132,132 @@ resource "aws_iam_role_policy" "terraform_permissions" {
           "s3:GetEncryptionConfiguration",
           "s3:GetBucketPublicAccessBlock",
           "s3:GetBucketOwnershipControls",
+          "s3:GetBucketNotification"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "CognitoPermissions"
+        Effect = "Allow"
+        Action = [
           "cognito-idp:DescribeUserPool",
           "cognito-idp:DescribeUserPoolClient",
           "cognito-idp:GetUserPoolMfaConfig",
+          "cognito-idp:DescribeUserPoolDomain",
+          "cognito-idp:GetGroup",
+          "cognito-idp:ListGroups"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "CloudFrontPermissions"
+        Effect = "Allow"
+        Action = [
           "cloudfront:GetDistribution",
           "cloudfront:GetDistributionConfig",
           "cloudfront:GetCachePolicy",
           "cloudfront:GetOriginAccessControl",
+          "cloudfront:GetResponseHeadersPolicy",
+          "cloudfront:ListCachePolicies",
+          "cloudfront:ListOriginRequestPolicies",
+          "cloudfront:GetCloudFrontOriginAccessIdentity",
+          "cloudfront:ListCloudFrontOriginAccessIdentities",
+          "cloudfront:GetOriginRequestPolicy",
+          "cloudfront:ListResponseHeadersPolicies"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "EventBridgePermissions"
+        Effect = "Allow"
+        Action = [
           "events:DescribeRule",
           "events:ListTargetsByRule",
+          "events:DescribeEventBus",
+          "events:ListTagsForResource"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "SQSPermissions"
+        Effect = "Allow"
+        Action = [
           "sqs:GetQueueAttributes",
           "sqs:GetQueueUrl",
+          "sqs:ListQueueTags"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "CloudWatchLogsPermissions"
+        Effect = "Allow"
+        Action = [
           "logs:DescribeLogGroups",
           "logs:ListTagsLogGroup",
+          "logs:ListTagsForResource"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "SESPermissions"
+        Effect = "Allow"
+        Action = [
           "ses:GetIdentityVerificationAttributes",
           "ses:GetIdentityDkimAttributes",
+          "ses:DescribeConfigurationSet",
+          "ses:GetIdentityMailFromDomainAttributes",
+          "ses:GetTemplate",
+          "ses:ListTemplates"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "WAFPermissions"
+        Effect = "Allow"
+        Action = [
           "wafv2:GetWebACL",
           "wafv2:ListTagsForResource",
+          "wafv2:GetLoggingConfiguration"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "Route53Permissions"
+        Effect = "Allow"
+        Action = [
           "route53:GetHostedZone",
           "route53:ListResourceRecordSets",
+          "route53:ListHostedZones",
+          "route53:ListHostedZonesByName"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "ACMPermissions"
+        Effect = "Allow"
+        Action = [
           "acm:DescribeCertificate",
           "acm:ListTagsForCertificate",
+          "acm:ListCertificates"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "SchedulerPermissions"
+        Effect = "Allow"
+        Action = [
           "scheduler:GetSchedule",
-          "scheduler:GetScheduleGroup"
+          "scheduler:GetScheduleGroup",
+          "scheduler:ListSchedules"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "CloudWatchPermissions"
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:DescribeAlarms",
+          "cloudwatch:ListTagsForResource"
         ]
         Resource = "*"
       }
@@ -138,7 +274,7 @@ resource "aws_iam_role_policy" "terraform_apply_permissions" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "TerraformApplyPermissions"
+        Sid    = "IAMApplyPermissions"
         Effect = "Allow"
         Action = [
           "iam:CreateRole",
@@ -155,6 +291,17 @@ resource "aws_iam_role_policy" "terraform_apply_permissions" {
           "iam:DeletePolicy",
           "iam:CreatePolicyVersion",
           "iam:DeletePolicyVersion",
+          "iam:CreateOpenIDConnectProvider",
+          "iam:DeleteOpenIDConnectProvider",
+          "iam:UpdateOpenIDConnectProviderThumbprint",
+          "iam:TagOpenIDConnectProvider"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "LambdaApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "lambda:CreateFunction",
           "lambda:DeleteFunction",
           "lambda:UpdateFunctionCode",
@@ -167,18 +314,39 @@ resource "aws_iam_role_policy" "terraform_apply_permissions" {
           "lambda:CreateAlias",
           "lambda:DeleteAlias",
           "lambda:UpdateAlias",
-          "lambda:PutFunctionConcurrency",
+          "lambda:PutFunctionConcurrency"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "APIGatewayApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "apigateway:POST",
           "apigateway:PUT",
           "apigateway:PATCH",
-          "apigateway:DELETE",
+          "apigateway:DELETE"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "DynamoDBApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "dynamodb:CreateTable",
           "dynamodb:DeleteTable",
           "dynamodb:UpdateTable",
           "dynamodb:TagResource",
           "dynamodb:UntagResource",
           "dynamodb:UpdateTimeToLive",
-          "dynamodb:UpdateContinuousBackups",
+          "dynamodb:UpdateContinuousBackups"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "S3ApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "s3:CreateBucket",
           "s3:DeleteBucket",
           "s3:PutBucketPolicy",
@@ -191,6 +359,14 @@ resource "aws_iam_role_policy" "terraform_apply_permissions" {
           "s3:PutEncryptionConfiguration",
           "s3:PutBucketPublicAccessBlock",
           "s3:PutBucketOwnershipControls",
+          "s3:PutBucketNotification"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "CognitoApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "cognito-idp:CreateUserPool",
           "cognito-idp:DeleteUserPool",
           "cognito-idp:UpdateUserPool",
@@ -200,53 +376,158 @@ resource "aws_iam_role_policy" "terraform_apply_permissions" {
           "cognito-idp:SetUserPoolMfaConfig",
           "cognito-idp:CreateUserPoolDomain",
           "cognito-idp:DeleteUserPoolDomain",
+          "cognito-idp:CreateGroup",
+          "cognito-idp:DeleteGroup",
+          "cognito-idp:UpdateGroup"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "CloudFrontApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "cloudfront:CreateDistribution",
           "cloudfront:DeleteDistribution",
           "cloudfront:UpdateDistribution",
           "cloudfront:CreateCachePolicy",
           "cloudfront:DeleteCachePolicy",
+          "cloudfront:UpdateCachePolicy",
           "cloudfront:CreateOriginAccessControl",
           "cloudfront:DeleteOriginAccessControl",
           "cloudfront:CreateInvalidation",
           "cloudfront:TagResource",
+          "cloudfront:CreateResponseHeadersPolicy",
+          "cloudfront:DeleteResponseHeadersPolicy",
+          "cloudfront:UpdateResponseHeadersPolicy",
+          "cloudfront:CreateCloudFrontOriginAccessIdentity",
+          "cloudfront:DeleteCloudFrontOriginAccessIdentity",
+          "cloudfront:UpdateCloudFrontOriginAccessIdentity",
+          "cloudfront:CreateOriginRequestPolicy",
+          "cloudfront:DeleteOriginRequestPolicy",
+          "cloudfront:UpdateOriginRequestPolicy"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "EventBridgeApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "events:PutRule",
           "events:DeleteRule",
           "events:PutTargets",
           "events:RemoveTargets",
           "events:TagResource",
+          "events:UntagResource",
+          "events:CreateEventBus",
+          "events:DeleteEventBus",
+          "events:UpdateEventBus"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "SQSApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "sqs:CreateQueue",
           "sqs:DeleteQueue",
           "sqs:SetQueueAttributes",
           "sqs:TagQueue",
-          "sqs:UntagQueue",
+          "sqs:UntagQueue"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "CloudWatchLogsApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "logs:CreateLogGroup",
           "logs:DeleteLogGroup",
           "logs:PutRetentionPolicy",
           "logs:TagLogGroup",
+          "logs:UntagLogGroup",
+          "logs:TagResource",
+          "logs:UntagResource"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "SESApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "ses:VerifyDomainIdentity",
           "ses:DeleteIdentity",
           "ses:VerifyDomainDkim",
           "ses:SetIdentityDkimEnabled",
           "ses:CreateEmailIdentity",
           "ses:DeleteEmailIdentity",
+          "ses:CreateConfigurationSet",
+          "ses:DeleteConfigurationSet",
+          "ses:SetIdentityMailFromDomain",
+          "ses:CreateTemplate",
+          "ses:DeleteTemplate",
+          "ses:UpdateTemplate",
+          "ses:VerifyEmailIdentity"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "WAFApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "wafv2:CreateWebACL",
           "wafv2:DeleteWebACL",
           "wafv2:UpdateWebACL",
           "wafv2:AssociateWebACL",
           "wafv2:DisassociateWebACL",
           "wafv2:TagResource",
+          "wafv2:UntagResource",
+          "wafv2:PutLoggingConfiguration",
+          "wafv2:DeleteLoggingConfiguration"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "Route53ApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "route53:ChangeResourceRecordSets",
           "route53:CreateHostedZone",
-          "route53:DeleteHostedZone",
+          "route53:DeleteHostedZone"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "ACMApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "acm:RequestCertificate",
           "acm:DeleteCertificate",
-          "acm:AddTagsToCertificate",
+          "acm:AddTagsToCertificate"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "SchedulerApplyPermissions"
+        Effect = "Allow"
+        Action = [
           "scheduler:CreateSchedule",
           "scheduler:DeleteSchedule",
           "scheduler:UpdateSchedule",
           "scheduler:CreateScheduleGroup",
           "scheduler:DeleteScheduleGroup",
-          "scheduler:TagResource"
+          "scheduler:TagResource",
+          "scheduler:UntagResource"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "CloudWatchApplyPermissions"
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:PutMetricAlarm",
+          "cloudwatch:DeleteAlarms",
+          "cloudwatch:TagResource",
+          "cloudwatch:UntagResource"
         ]
         Resource = "*"
       }
@@ -255,3 +536,4 @@ resource "aws_iam_role_policy" "terraform_apply_permissions" {
 }
 
 data "aws_caller_identity" "current" {}
+
